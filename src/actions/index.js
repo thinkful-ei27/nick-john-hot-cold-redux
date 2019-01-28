696d52375a7bf0e766bf22a0d40524b0882dd0be
John Young <john.young@spreadstreet.io>
2019-01-28 14:06:34 -0500

    First commit

---
+++ src/actions/index.js
@@ -0,0 +1,13 @@
+// Restart game
+// Make a guess
+
+export const RESTART_GAME = 'RESTART_GAME';
+export const restartGame = () => ({
+    type: RESTART_GAME
+})
+
+export const MAKE_GUESS = 'MAKE_GUESS';
+export const makeGuess = () => ({
+    type: MAKE_GUESS
+})
+
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import 
+
+const initialState = {
+    guesses: [],
+    feedback: 'Make your guess!',
+    auralStatus: '',
+    correctAnswer: Math.floor(Math.random() * 100) + 1
+  };
